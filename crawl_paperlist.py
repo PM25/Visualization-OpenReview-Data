5fb49cfede8bb1ca9b428ae1c8d8d90aed6687d3
PY <pyhuang97@gmail.com>
2023-03-10 12:41:45 +0800

    seperate accepted and rejected papers.

--- crawl_paperlist.py
+++ crawl_paperlist.py
@@ -1,16 +1,16 @@
 import time
 from tqdm import tqdm
-import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+service = Service("./chromedriver")
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
 def crawl_paperlist(conference, year, presentation_type):
@@ -23,7 +23,7 @@ def crawl_paperlist(conference, year, presentation_type):
     WebDriverWait(driver, 60).until(cond)
 
     print("Downloading")
-    success_results = fail_results = []
+    success_results = []
     elements = driver.find_elements(By.XPATH, f'//*[@id="{presentation_type}"]/ul/li')
 
     for i, element in enumerate(tqdm(elements)):
@@ -47,11 +47,22 @@ def crawl_paperlist(conference, year, presentation_type):
             success_results.append([paper_id, year, title, link, presentation_type, keyword, abstract])
 
         except Exception as e:
-            print(f"failed to download #{i}", e)
-            fail_results.append(i)
+            print(f"{conf}{year}_{presentation_type}#{i} failed", e)
             continue
 
-    return success_results, fail_results
+    return success_results
+
+
+def check_type(_type):
+    accepted_keywords = ["accepted", "spotlight", "oral", "poster"]
+    rejected_keywords = ["rejected", "withdrawn"]
+    for acc_keyword in accepted_keywords:
+        if acc_keyword in _type:
+            return "accepted"
+    for rej_keyword in rejected_keywords:
+        if rej_keyword in _type:
+            return "rejected"
+    return None
 
 
 def get_presentation_types(conference, year):
@@ -59,25 +70,32 @@ def get_presentation_types(conference, year):
     driver.get(link)
     elements = driver.find_elements(By.XPATH, f'//*[@role="presentation"]/a')
     types = [e.get_attribute("href").split("#")[1] for e in elements]
-    types.remove("your-consoles") if "your-consoles" in types else None
-    return types
+    accepted_types = [t for t in types if check_type(t) == "accepted"]
+    rejected_types = [t for t in types if check_type(t) == "rejected"]
+    return accepted_types, rejected_types
 
 
-conferences = {"ICLR": ["2018", "2019", "2020", "2021", "2022"], "NeurIPS": ["2021", "2022", "2023"]}
+conferences = {"ICLR": ["2018", "2019", "2020", "2021", "2022"], "NeurIPS": ["2021", "2022"]}
+
 
 for conf, years in conferences.items():
     for year in years:
-        presentation_types = get_presentation_types(conf, year)
+        accepted_types, rejected_types = get_presentation_types(conf, year)
+        accepted_results = rejected_results = []
+        for _type in accepted_types:
+            results = crawl_paperlist(conf, year, _type)
+            accepted_results.extend(results)
+        for _type in accepted_types:
+            results = crawl_paperlist(conf, year, _type)
+            rejected_results.extend(results)
 
-        success_results = failed_results = []
-        for _type in presentation_types:
-            success, failed = crawl_paperlist(conf, year, _type)
-            success_results.extend(success)
-            failed_results.extend(failed)
-
-    with open(f"{conf}_paper_list.tsv", "w", encoding="utf8") as f:
+    with open(f"{conf}_accepted_paper_list.tsv", "w", encoding="utf8") as f:
         f.write("\t".join(["paper_id", "year", "title", "link", "presentation_type", "keywords", "abstract"]) + "\n")
-        f.write("\n".join(["\t".join(result) for result in success_results]))
+        f.write("\n".join(["\t".join(result) for result in accepted_results]))
+
+    with open(f"{conf}_rejected_paper_list.tsv", "w", encoding="utf8") as f:
+        f.write("\t".join(["paper_id", "year", "title", "link", "presentation_type", "keywords", "abstract"]) + "\n")
+        f.write("\n".join(["\t".join(result) for result in rejected_results]))
 
 driver.close()
-print("File saved to paper_list.tsv")
+print(f"Finished!")
